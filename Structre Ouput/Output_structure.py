db6aa7266e7d078c74dc40684b212981b19709a5
Arul <ajay@Ajays-MacBook-Pro.local>
2025-03-14 15:25:56 +0530

    Modify into getting all details in a final response

--- Structre Ouput/Output_structure.py
+++ Structre Ouput/Output_structure.py
@@ -1,11 +1,15 @@
 from pydantic import BaseModel,Field
+from typing import List
 from langgraph.graph.message import MessagesState
 
-class FinalResponse(BaseModel):
+class FlightDetails(BaseModel):
     """Responed to user like this"""
     Departure_AirportName: str = Field(description="It descripes departure airport name")
     Arrival_AirportName : str = Field(description="it descripes arrival airport name")
     price : int = Field(description="it descripes the price")
+
+class FinalResponse(FlightDetails):
+    flights : List[FlightDetails]
 
 class AgentState(MessagesState):
 
